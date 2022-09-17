cb5f861db815172eb4b6cbcb053c1b8aa4ef63be
Artem <slivnitsin.art@mail.ru>
2022-09-17 23:28:52 +0800

    task_61

---
+++ task_61/Program.cs
@@ -0,0 +1,20 @@
+﻿// Вывести первые N строк треугольника Паскаля. 
+// Сделать вывод в виде равнобедренного треугольника.
+
+Console.Write("Введите нужное количество строк треугольника Паскаля: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int factorial(int N)
+{
+    int x = 1;
+    for (int i = 1; i <= N; i++) x *= i;
+    return x;
+}
+
+for (int i = 0; i < N; i++)
+{
+    for (int c = 0; c <= (N - i); c++) Console.Write(" "); 
+    for (int c = 0; c <= i; c++) Console.Write(" " + (factorial(i) / (factorial(c) * factorial(i - c))));
+    Console.WriteLine();
+    Console.WriteLine();
+}
