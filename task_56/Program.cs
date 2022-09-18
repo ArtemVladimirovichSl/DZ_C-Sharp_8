3c03adbec4df0ac6d01692328ca0b98c4277ab9a
Artem <slivnitsin.art@mail.ru>
2022-09-18 11:56:58 +0800

    task_56.1

--- task_56/Program.cs
+++ task_56/Program.cs
@@ -6,33 +6,26 @@ int columns = new Random().Next(3,7);
 int[,] matrix = new int[rows, columns];
 int sum = int.MaxValue;
 int index = 0;
-
-if (rows != columns)
-{            
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            matrix[i,j] = new Random().Next(10);
-            Console.Write(matrix[i,j] + " ");
-        }
-        Console.WriteLine();
-    }
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        int temp = 0;
-        for (int j = 0; j < matrix.GetLength(1); j++) temp += matrix[i, j];
-        if (temp < sum)
-        {
-            sum = temp;
-            index = i;
-        }
-    }
-    Console.WriteLine("Строка: "+ (index + 1) +" Сумма - "+sum);
-    Console.WriteLine();
-    for (int i = 0; i < matrix.GetLength(1); i++) Console.Write(matrix[index, i]+" ");
-}
-else
+        
+for (int i = 0; i < matrix.GetLength(0); i++)
 {
-Console.Write("Массив не прямоугольный");
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = new Random().Next(10);
+        Console.Write(matrix[i,j] + " ");
+    }
+Console.WriteLine();
 }
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    int temp = 0;
+    for (int j = 0; j < matrix.GetLength(1); j++) temp += matrix[i, j];
+    if (temp < sum)
+    {
+        sum = temp;
+        index = i;
+    }
+}
+Console.WriteLine("Строка: "+ (index + 1) +" Сумма - "+sum);
+Console.WriteLine();
+for (int i = 0; i < matrix.GetLength(1); i++) Console.Write(matrix[index, i]+" ");
